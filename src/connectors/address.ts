51e5b6058ef4b1e7dcaa0b6a2d86baac0d1cc6f8
Shivam Singh <shivamsingh071093@gmail.com>
2022-06-23 21:32:31 +0530

    update

--- src/connectors/address.ts
+++ src/connectors/address.ts
@@ -1,2 +1,2 @@
-export const NFTContract = "0x58C218202070fdcb6397612DA286c1aB6Dd9524F";
-export const rpc = "https://goerli.infura.io/v3/8f5f536cb9d54ee1afa38e2fa9fb258a";
+export const NFTContract = "0xc1246288eC96F627e5bB24C4E8825cC600cE8F16";
+export const rpc = "https://mainnet.infura.io/v3/8f5f536cb9d54ee1afa38e2fa9fb258a";
